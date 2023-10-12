9cb5ddc4a1f821ffdffa4ae399b90807f2a80a6b
AB <bourdeaux.adrien@gmail.com>
2023-10-12 11:43:50 -0400

    added working image carousel

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+/* TODO: 
+- Crop all images into 1 standard size
+- Make carousel fit inside viewport
+*/
 function App() {
   return (
     <><center><h1 className="InPageTitle">WW1 Primary Sources - Paintings</h1></center>
-    <div id="carousel-sample" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carousel-sample" data-bs-slide-to="0" className="active"></button>
-    <button type="button" data-bs-target="#carousel-sample" data-bs-slide-to="1"></button>
-    <button type="button" data-bs-target="#carousel-sample" data-bs-slide-to="2"></button>
-    <button type="button" data-bs-target="#carousel-sample" data-bs-slide-to="3"></button>
-    <button type="button" data-bs-target="#carousel-sample" data-bs-slide-to="4"></button>
+
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
   </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img className="d-block w-100" alt="" src="/img/vimy-monument.jpg" />
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/img/vimy-monument.jpg" class="d-block w-100" alt="..."></img>
     </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" alt="" src="/img/the-taking-of-vimy-ridge.jpg" />
+    <div class="carousel-item">
+      <img src="/img/the-crest-of-vimy-ridge.jpg" class="d-block w-100" alt="..."></img>
     </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" alt="" src="/img/the-crest-of-vimy-ridge.jpg" />
+    <div class="carousel-item">
+      <img src="/img/the-taking-of-vimy-ridge.jpg" class="d-block w-100" alt="..."></img>
     </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" alt="" src="/img/cemetary-on-vimy-ridge" />
+    <div class="carousel-item">
+      <img src="/img/cemetary-on-vimy-ridge.jpg" class="d-block w-100" alt="..."></img>
     </div>
   </div>
-  <a className="carousel-control-prev" data-bs-target="#carousel-sample" role="button" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </a>
-  <a className="carousel-control-next" data-bs-target="#carousel-sample" role="button" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </a>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
 </div>
 </>
   );
