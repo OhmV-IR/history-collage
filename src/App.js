0cc1a2109a66834e047c0d63f127c42a773af39f
AB <bourdeaux.adrien@gmail.com>
2023-10-12 12:51:31 -0400

    correctly sized images

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 - Resize all images into 1 standard size
 - Make carousel fit inside viewport
 */
+const showInference1 = async event => {
+
+}
+const showInference2 = async event => {
+  
+}
+const showInference3 = async event => {
+  
+}
+const showInference4 = async event => {
+  
+}
 function App() {
   return (
-    <><center><h1 className="InPageTitle">WW1 Primary Sources - Paintings</h1></center>
+    <>
 <div id="carouselExampleIndicators" className="carousel slide">
   <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
@@ -15,16 +27,16 @@ function App() {
   </div>
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <img src="/img/vimy-monument-cropped.jpg" className="d-block w-100" alt="..."></img>
+    <img src="/img/vimy-monument-cropped.jpg" onClick={showInference1} className="d-block w-100 limitImageSize" alt="The monument of Vimy Ridge with the ghosts of soldiers marching up the hill"></img>
     </div>
     <div className="carousel-item">
-      <img src="/img/crest-of-vimy-ridge-cropped.jpg" className="d-block w-100" alt="..."></img>
+    <img src="/img/crest-of-vimy-ridge-cropped.jpg" onClick={showInference2} className="d-block w-100 limitImageSize" alt="A lone soldier walking on Vimy Ridge"></img>
     </div>
     <div className="carousel-item">
-      <img src="/img/the-taking-of-vimy-ridge-cropped.jpg" className="d-block w-100" alt="..."></img>
+    <img src="/img/the-taking-of-vimy-ridge-cropped.jpg" onClick={showInference3} className="d-block w-100 limitImageSize" alt="Depicts the battle of vimy ridge and a field gun firing on German positions"></img>
     </div>
     <div className="carousel-item">
-      <img src="/img/cemetary-on-vimy-ridge-cropped.jpg" className="d-block w-100" alt="..."></img>
+      <img src="/img/cemetary-on-vimy-ridge-cropped.jpg" onClick={showInference4} onMouseDownclassName="d-block w-100 limitImageSize" alt="A cemetary on the battlefield of Vimy Ridge"></img>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
