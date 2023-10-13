f30ac835bb5968432e5db48b4bb3ac4a2bb0344c
AB <bourdeaux.adrien@gmail.com>
2023-10-13 09:57:33 -0400

    added displaying for inferences on click of images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import './App.css';
+import React, { useState } from 'react';
 /* TODO: 
 - Make popup show up and write inferences
 */
-const showInference1 = async event => {
-
-}
-const showInference2 = async event => {
-  
-}
-const showInference3 = async event => {
-  
-}
-const showInference4 = async event => {
-  
-}
 function App() {
+  const showInference1 = async event => {
+    console.log("Show / hide inference 1");
+    changeInference1Visibility(!displayInference1);
+  }
+  const showInference2 = async event => {
+    console.log("Show / hide inference 2");
+    changeInference2Visibility(!displayInference2);
+  }
+  const showInference3 = async event => {
+    console.log("Show / hide inference 3");
+    changeInference3Visibility(!displayInference3);
+  }
+  const showInference4 = async event => {
+    console.log("Show / hide inference 4");
+    changeInference4Visibility(!displayInference4);
+  }
+  const [displayInference1, changeInference1Visibility] = useState(false);
+  const [displayInference2, changeInference2Visibility] = useState(false);
+  const [displayInference3, changeInference3Visibility] = useState(false);
+  const [displayInference4, changeInference4Visibility] = useState(false);
   return (
+    
     <>
 <div id="carouselExampleIndicators" className="carousel slide">
   <div className="carousel-indicators">
@@ -25,17 +35,17 @@ function App() {
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
   </div>
   <div className="carousel-inner">
-    <div className="carousel-item active">
-    <img src="/img/vimy-monument-cropped.jpg" onClick={showInference1} className="d-block w-100 limitImageSize" alt="The monument of Vimy Ridge with the ghosts of soldiers marching up the hill"></img>
+    <div className="carousel-item active" onClick={showInference1}>
+    <img src="/img/vimy-monument-cropped.jpg" className="d-block w-100 limitImageSize" alt="The monument of Vimy Ridge with the ghosts of soldiers marching up the hill"></img>
     </div>
-    <div className="carousel-item">
-    <img src="/img/crest-of-vimy-ridge-cropped.jpg" onClick={showInference2} className="d-block w-100 limitImageSize" alt="A lone soldier walking on Vimy Ridge"></img>
+    <div className="carousel-item" onClick={showInference2}>
+    <img src="/img/crest-of-vimy-ridge-cropped.jpg" className="d-block w-100 limitImageSize" alt="A lone soldier walking on Vimy Ridge"></img>
     </div>
-    <div className="carousel-item">
-    <img src="/img/the-taking-of-vimy-ridge-cropped.jpg" onClick={showInference3} className="d-block w-100 limitImageSize" alt="Depicts the battle of vimy ridge and a field gun firing on German positions"></img>
+    <div className="carousel-item" onClick={showInference3}>
+    <img src="/img/the-taking-of-vimy-ridge-cropped.jpg" className="d-block w-100 limitImageSize" alt="Depicts the battle of vimy ridge and a field gun firing on German positions"></img>
     </div>
-    <div className="carousel-item">
-      <img src="/img/cemetary-on-vimy-ridge-cropped.jpg" onClick={showInference4} className="d-block w-100 limitImageSize" alt="A cemetary on the battlefield of Vimy Ridge"></img>
+    <div className="carousel-item" onClick={showInference4}>
+      <img src="/img/cemetary-on-vimy-ridge-cropped.jpg" className="d-block w-100 limitImageSize" alt="A cemetary on the battlefield of Vimy Ridge"></img>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -46,6 +56,24 @@ function App() {
     <span className="carousel-control-next-icon" aria-hidden="true"></span>
     <span className="visually-hidden">Next</span>
   </button>
+</div>
+<div className="inferences">
+        {displayInference1
+          ?   <h1 id="inference1">Inference 1</h1>
+          : <></>
+      }
+      {displayInference2
+          ?   <h1 id="inference2">Inference 2</h1>
+          : <></>
+      }
+      {displayInference3
+          ?   <h1 id="inference3">Inference 3</h1>
+          : <></>
+      }
+      {displayInference4
+          ?   <h1 id="inference4">Inference 4</h1>
+          : <></>
+      }
 </div>
 </>
   );
